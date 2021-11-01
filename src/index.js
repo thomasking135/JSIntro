67b17f20c6a28b59a9d44dcabd2fc48653d8006f
Joshua Vial <joshua@enspiral.com>
2021-11-02 12:23:55 +1300

    refactored, put in tests for results

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const variableAnswers = require("./challenge/1-variables");
+const functionAnswers = require("./challenge/2-functions");
+
+const { formatResultsSection } = require("./lib");
+const { resultsForVariables, resultsForFunctions } = require("./lib/results");
 
 document.addEventListener("DOMContentLoaded", () => {
   const el = document.getElementById("results");
@@ -7,48 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function buildResults() {
   let variableResults = resultsForVariables(variableAnswers);
+  let functionResults = resultsForFunctions(functionAnswers);
+
   let output = "<h1>Results</h1>";
   output += formatResultsSection("Variables", variableResults);
+  output += formatResultsSection("Functions", functionResults);
   return output;
-}
-
-function formatResultsSection(title, results) {
-  let output = `<div class='results'>
-    <h2>${title}</h2>
-    <ol>
-    ${formatResults(results)}
-    </ol>
-  </div>`;
-  return output;
-}
-
-function formatResults(results) {
-  return results.map(formatResult).join("");
-}
-
-function formatResult({ correct, response, tip }) {
-  return `<li class="${correct ? "tick" : "cross"}">
-    ${response}
-    ${correct ? "" : tip}
-    </li>`;
-}
-
-function resultsForVariables({ welcome, name, value }) {
-  return (results = [
-    {
-      correct: welcome === "Hello world",
-      response: welcome,
-      tip: welcome !== undefined ? "tip goes here" : "",
-    },
-    {
-      correct: typeof name === "string" && name != "Old Name",
-      response: name,
-      tip: name !== "Old Name" ? "tip goes here" : "",
-    },
-    {
-      correct: value === "Hardy",
-      response: value,
-      tip: value !== undefined ? "Nearly, try reading through it again" : "",
-    },
-  ]);
 }
