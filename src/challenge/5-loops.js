65b27468115755656ca78976867ed0eba81bec0f
emilyparkes <emilycoco@me.com>
2021-11-15 16:49:05 +1300

    remove nested loop

--- src/challenge/5-loops.js
+++ src/challenge/5-loops.js
@@ -9,15 +9,6 @@ function logNames (names) {
   }
 }
 
-//  -- nested loop? --
-let a = [[1, 7, 3], [2, 8, 5], [9, 0, 4]];
-let sum = 0;
-for (let i = 0; i < 3; i++) {
-  for (let j = 0; j < 3; j++) {
-    sum = sum + a[i][j];
-  }
-}
-
 // ------------ CHECKS -----------
 // The below line of code lets us test it to see how you're doing,
 // don't worry about this yet you'll learn it in Bootcamp. 
