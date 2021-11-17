7211f0e766cb8fb968fca453121cba338ea61bf3
Joshua Vial <joshua@enspiral.com>
2021-11-18 12:39:51 +1300

    test for loop results

--- src/challenge/5-loops.js
+++ src/challenge/5-loops.js
@@ -1,6 +1,6 @@
 //  -- for loop --
 // Write a function `logNames` that takes an array as a parameter. 
-// It should loop for the length of the array and console.log the current position. 
+// It should loop for the length of the array and console.log the name at current position. 
 // logNames(['Sally', 'Nikita', 'Arnav', 'Akira']) would console.log each person's name. 4 names in total.
 // logNames(['Arnav', 'Akira']) would console.log each person's name. 2 names in total.
 function logNames (names) {
